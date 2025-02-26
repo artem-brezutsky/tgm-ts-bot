f7cdee301585279a53331b8b27abff8a05534a8e
brezitskiy <brezitskiy@rozetka.ua>
2025-02-27 00:12:08 +0200

    commit

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Telegraf } from 'telegraf';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class BotService {}
+export class BotService implements OnModuleInit {
+    private bot: Telegraf;
+
+    constructor(private configService: ConfigService) {
+        const token = this.configService.get<string>('TELEGRAM_BOT_TOKEN');
+
+        if(!token) {
+            throw new Error('Bad token!');
+        }
+
+        console.log(token);
+        this.bot = new Telegraf(token);
+    }
+
+    async onModuleInit() {
+        this.bot.start(ctx => {
+            ctx.reply('Привет! Отправь мне ссылку и я скажу откуда она!');
+        });
+
+        this.bot.launch();
+        console.log('🤖 Бот запущен!');
+    }
+}
